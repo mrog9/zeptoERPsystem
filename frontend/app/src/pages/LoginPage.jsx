761a00a85d019c6c637a0545c7f034b4d50a4e13
mwr2 <mwr2@clemson.edu>
2026-05-29 10:34:18 -0400

    adding login form to login page

--- frontend/app/src/pages/LoginPage.jsx
+++ frontend/app/src/pages/LoginPage.jsx
@@ -1,8 +1,18 @@
+import { LoginForm } from "../components/LoginForm.jsx"
+
 export function LoginPage(){
   
   return (
     
-    <h1>Login Page</h1>
+    <div id="LoginPage">
+
+      <h1>WELCOME TO ZEPTO GROCERIES!</h1>
+
+      <h2> SIGN IN TO CONTINUE</h2>
+
+      <LoginForm />
+
+    </div>
 
   );
 }
