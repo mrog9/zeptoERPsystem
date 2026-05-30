9ae06e896fa3eb5eb57cc30f070972056ecdd7cb
mwr2 <mwr2@clemson.edu>
2026-05-30 12:14:05 -0400

    using one browser to run tests

--- zeptotests/playwright/tests/example.spec.ts
+++ zeptotests/playwright/tests/example.spec.ts
@@ -1,9 +1,15 @@
 import { test, expect } from '@playwright/test';
+import { chromium } from 'playwright';
 
-test('has title', async ({ page }) => {
+(async () => {
+
+  const browser = await chromium.launch();
+
+  const page = await browser.newPage();
+
   await page.goto('http://localhost:5173');
 
-  const btn = await page.getByRole('button')
+  const btn = await page.getByRole('button');
 
-  await expect(btn).toBeVisible()
-});
+  await expect(btn).toBeVisible();
+})();
